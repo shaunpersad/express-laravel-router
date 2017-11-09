9211ca6a6da75f3b45ef07d1511fb65beee34e40
shaunpersad <shaunpersad@gmail.com>
2017-11-08 23:28:52 -0500

    added paramsFromUri

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const createRouter = require('./lib/createRouter');
 const laravelToExpress = require('./lib/laravelToExpress');
 const uriWithParams = require('./lib/uriWithParams');
+const paramsFromUri = require('./lib/paramsFromUri');
 
 module.exports = {
     createRouter,
     laravelToExpress,
-    uriWithParams
+    uriWithParams,
+    paramsFromUri
 };
